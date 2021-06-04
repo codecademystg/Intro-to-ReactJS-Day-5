e593050a33fa3a4847953684d0015788128e9900
debajit13 <mallickdebajit3@gmail.com>
2021-06-04 20:01:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Name from "./components/Name";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Name name="John Doe" age="18" />
+      <Name name="Jane Doe" age="50" />
+      <Name name="Debajit" age="20" />
+      <Name name="John Doe" age="18" />
     </div>
   );
 }
---
+++ src/components/Name.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// function Name({ name, age }) {
+//   return (
+//     <div>
+//       <h2>{name}</h2>
+//       <h3>Age: {age}</h3>
+//       <p>Lorem ipsum dolor</p>
+//     </div>
+//   );
+// }
+
+const Name = ({ name, age }) => (
+  <div>
+    <h2>{name}</h2>
+    <h3>Age: {age}</h3>
+    <p>Lorem ipsum dolor</p>
+  </div>
+);
+
+export default Name;
